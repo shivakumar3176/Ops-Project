9c12ce91ddf6556bb10f367a78b2ee051f68bff5
shiva <nanishiva3176@gmail.com>
2025-07-22 10:00:56 +0530

    Update API URL for deployment

--- olx/frontend/src/api.js
+++ olx/frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:3000/api',
+  baseURL: 'https://ops-backend-cayb.onrender.com/api',
 });
 
 API.interceptors.request.use((req) => {
